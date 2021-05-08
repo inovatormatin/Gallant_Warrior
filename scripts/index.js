080bd195746af0571d01ca55ccfd4e44998b242c
inovatormatin <manojgeetparmar@gmail.com>
2021-05-08 16:34:22 +0530

    sub ok

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,11 @@
-// empty
+let perks_section = document.getElementById("perks_section");
+let donate_btn = document.getElementById("donate_btn");
+function on_off_perk() {
+  if (perks_section.style.display === "none") {
+    perks_section.style.display = "block";
+  }
+  else {
+    perks_section.style.display = "none";
+  }
+}
+donate_btn.addEventListener("click", on_off_perk);
